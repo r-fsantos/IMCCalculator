bbc1990ad6d5fcf5253f9343d4996a5c01a36403
renato <felicio95.jr@gmail.com>
2022-03-12 10:29:24 -0300

    :tada: Project initial commit.

---
+++ IMCCalculator/IMCCalculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  IMCCalculator
+//
+//  Created by Renato F. dos Santos Jr on 12/03/22.
+//
+
+/*
+ - Story board
+ - Ligação
+ - Recuperar os valores
+ - chamar uma controller
+ - Recuperar o valor
+ - Retornar
+ */
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
